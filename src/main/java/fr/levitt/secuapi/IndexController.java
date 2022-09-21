ca6c4223f16dfd7f4db9fe31358df64a645b46de
Olivier Levitt <olivier.levitt@gmail.com>
2022-09-21 11:31:58 +0200

    Endpoint utilisateur

--- src/main/java/fr/levitt/secuapi/IndexController.java
+++ src/main/java/fr/levitt/secuapi/IndexController.java
@@ -1,5 +1,6 @@
 package fr.levitt.secuapi;
 
+import fr.levitt.secuapi.core.Utilisateur;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     @GetMapping("/")
-    public void index() {
-
+    public Utilisateur index() {
+        Utilisateur utilisateur = new Utilisateur();
+        utilisateur.setNom("Toto");
+        utilisateur.setPrenom("Tata");
+        return utilisateur;
     }
 }
---
+++ src/main/java/fr/levitt/secuapi/core/Utilisateur.java
@@ -0,0 +1,23 @@
+package fr.levitt.secuapi.core;
+
+public class Utilisateur {
+
+    private String nom;
+    private String prenom;
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+}
