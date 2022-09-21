fe0ea6e5bbdde90ad705c7ff4f9e27ebdb98a84d
Olivier Levitt <olivier.levitt@gmail.com>
2022-09-21 10:15:37 +0200

    Initial commit

---
+++ src/main/java/fr/levitt/secuapi/SecuApiApplication.java
@@ -0,0 +1,13 @@
+package fr.levitt.secuapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/levitt/secuapi/SecuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.levitt.secuapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
