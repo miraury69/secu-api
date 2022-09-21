2c78c6b10d34d5ee9c2317394978de2cb8663a38
Olivier Levitt <olivier.levitt@gmail.com>
2022-09-21 13:43:50 +0200

    Ajout table Utilisateur

--- src/main/java/fr/levitt/secuapi/core/Utilisateur.java
+++ src/main/java/fr/levitt/secuapi/core/Utilisateur.java
@@ -1,9 +1,23 @@
 package fr.levitt.secuapi.core;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Utilisateur {
 
+    @Id
+    private int idUtilisateur;
     private String nom;
     private String prenom;
+
+    public void setIdUtilisateur(int idUtilisateur) {
+        this.idUtilisateur = idUtilisateur;
+    }
+
+    public int getIdUtilisateur() {
+        return idUtilisateur;
+    }
 
     public void setNom(String nom) {
         this.nom = nom;
