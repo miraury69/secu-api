0d59cc8eb2a5372052c26dbfdbcbb7b3bef4c953
Olivier Levitt <olivier.levitt@gmail.com>
2022-09-21 11:21:47 +0200

    First controller

---
+++ src/main/java/fr/levitt/secuapi/IndexController.java
@@ -0,0 +1,13 @@
+package fr.levitt.secuapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public void index() {
+
+    }
+}
