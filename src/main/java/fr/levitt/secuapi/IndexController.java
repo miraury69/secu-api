57432a52a73e933f89f7b473828a08fd33d87ee3
Olivier Levitt <olivier.levitt@gmail.com>
2022-09-26 15:15:33 +0200

    Basic auth example

--- src/main/java/fr/levitt/secuapi/IndexController.java
+++ src/main/java/fr/levitt/secuapi/IndexController.java
@@ -2,8 +2,14 @@ package fr.levitt.secuapi;
 
 import fr.levitt.secuapi.core.Utilisateur;
 import fr.levitt.secuapi.sql.UtilisateurRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -14,6 +20,18 @@ public class IndexController {
 
     @Autowired
     private UtilisateurRepository utilisateurRepository;
+
+    @GetMapping("/secure")
+    public ResponseEntity secure(@RequestHeader(value=HttpHeaders.AUTHORIZATION, required = false) String authorization) {
+        // Que les utilisateurs authentifiés
+        if (authorization == null) {
+            return new ResponseEntity<>("Hello World!", HttpStatus.UNAUTHORIZED);
+        }
+        else {
+            // TODO : verifier header
+            System.out.println(authorization);
+        }
+    }
 
 
     @GetMapping("/newUser")
